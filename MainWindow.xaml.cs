b1aa2886d95f8f894a72cd3d99ad776c708ce6a4
Gambda-II <alexanderlorengel_tgd@hotmail.de>
2024-01-18 16:54:13 +0100

    Finished CafeGUI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.CodeDom;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
 using System.Timers;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CafeGUI;
 
@@ -20,6 +22,7 @@ namespace CafeGUI;
 public partial class MainWindow : Window
 {
     int sum = 0;
+    int globalCounter = 0;
     const int specialPrice = 300, cappuccinoPrice = 400;
 
     public MainWindow()
@@ -31,7 +34,7 @@ public partial class MainWindow : Window
 
     public void StartUp()
     {
-        tbPreparing.Visibility = Visibility.Hidden;
+        //tbPreparing.Visibility = Visibility.Hidden;
     }
 
     private void Button_MouseEnter(object sender, MouseEventArgs e)
@@ -69,6 +72,7 @@ public partial class MainWindow : Window
     {
         sum += value;
         tbDisplayMessage.Text = "";
+        string sign = "";
 
         if (value < 0)
         {
@@ -77,15 +81,16 @@ public partial class MainWindow : Window
         else
         {
             tbDisplayMessage.Foreground = Brushes.LimeGreen;
+            sign = "+";
         }
 
         if (value >= 100 || value <= -100)
         {
-            tbDisplayMessage.Text = $" {value / 100} Euro";
+            tbDisplayMessage.Text = $" {sign} {value / 100} Euro";
         }
         else
         {
-            tbDisplayMessage.Text = $" {value} Cent";
+            tbDisplayMessage.Text = $" {sign} {value} Cent";
         }
         UpdateTotal();
     }
@@ -123,26 +128,97 @@ public partial class MainWindow : Window
     }
 
     private bool IsSumBiggerThanPrice(int price)
-    {
+    {   
+        
         return price <= sum;
     }
 
     private void PrepareCoffee()
     {
-        tbPreparing.Visibility = Visibility.Visible;
-        tbPreparing.Text = "Wird zubereitet! Bitte warten.";
+        //tbPreparing.Visibility = Visibility.Visible;
+        tbPreparing.Text = $"Wird zubereitet! Bitte warten. \n{globalCounter * 10} %";
 
         // Wait
-        StartMachine();
+        var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        StartMachine(dispatcherTimer);
 
 
-        tbDisplayMessage.Text = "Bitteschön!";
-        tbPreparing.Visibility = Visibility.Hidden;
+        //tbDisplayMessage.Text = "Bitteschön!";
+        //tbPreparing.Visibility = Visibility.Hidden;
     }
 
-    private async void StartMachine()
+    private void StartMachine(DispatcherTimer dispatcherTimer)
     {
 
+        SetButtonsVisibility(Visibility.Hidden);
+
+        ChangeBackground("brewing");
+        int secondsToIdle = 1;
+        tbDisplayMessage.HorizontalAlignment = HorizontalAlignment.Left;
+        dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+        dispatcherTimer.Interval = TimeSpan.FromSeconds(secondsToIdle);
+        dispatcherTimer.Start();
+
+        //dispatcherTimer.IsEnabled = false;
+    }
+
+    private void ChangeBackground(string v)
+    {
+        ImageBrush image = new ImageBrush();
+        if (v == "brewing")
+        {
+
+        image.ImageSource = new BitmapImage(new Uri("A:\\GrundlagenOOP\\GUII\\coffee BG new brewing.png", UriKind.Relative));
+        }
+        else
+        {
+            image.ImageSource = new BitmapImage(new Uri("A:\\GrundlagenOOP\\GUII\\coffee BG new.png", UriKind.Relative));
+        }
+        wndMain.Background = image;
+    }
+
+    private void SetButtonsVisibility(Visibility v)
+    {
+        if (v == Visibility.Visible || v == Visibility.Hidden)
+        {
+            cnvMain.Children.OfType<Canvas>().ToList().ForEach(c => c.Children.OfType<Button>().ToList().ForEach(b => b.Visibility = v));
+        }
+    }
+
+    private void StopMachine(DispatcherTimer dispatcherTimer)
+    {
+        ChangeBackground("reset");
+        dispatcherTimer.Stop();
+        dispatcherTimer.IsEnabled = false;
+        tbDisplayMessage.Text = "Bitteschön!";
+        globalCounter = 0;
+
+        SetButtonsVisibility(Visibility.Visible);
+    }
+
+
+    private void dispatcherTimer_Tick(object? sender, EventArgs e)
+    {
+        // ▓▒░█
+        tbDisplayMessage.Text = "";
+        string loadingBar = "";//"░░░░░░░░░░";
+        globalCounter++;
+
+        tbPreparing.Text = $"Wird zubereitet! Bitte warten. \n{Math.Max(globalCounter * 10,0)} %";
+
+        for (int i = 1; i <= globalCounter; i++)
+        {
+            loadingBar += "█";
+
+        }
+            tbDisplayMessage.Text = loadingBar.PadRight(10 , '░');
+
+        if (globalCounter == 10)
+        {
+            StopMachine(sender as DispatcherTimer);
+            return;
+        }
+        //tbPreparing.Text += "\nSchubidubidu";
     }
 
 }
